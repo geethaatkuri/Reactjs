1a8b70b66ad517b10eecd29a1bad1f440292225e
Geetha <atkurigeetha910@gmail.com>
2024-05-17 16:45:31 +0530

    Adding Learnt files and Practice Problems

---
+++ JSX Challenges/practiceproblems/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+
+// Practice Problem 1 -> Series list in an ordered list
+
+// ReactDOM.render(
+//   <>
+//   <h1>5 Fav Netflix Series Pick</h1>
+//   <p>Here goes the list...</p>
+//   <ol>
+//     <li>Stranger Things</li>
+//     <li>Friends</li>
+//     <li>Fruit Basket</li>
+//     <li>Death-note</li>
+//     <li>Over the sky</li>
+//   </ol>
+//   </>,
+//   document.getElementById("root")
+  
+// );
+
+
+// Practice Problem 2 -> Displaying current date and time
+// const currDate = new Date().toLocaleDateString();
+// const currTime = new Date().toLocaleTimeString();
+// ReactDOM.render(
+// <>
+//   <h1>Displaying current date and time</h1>
+//   <p>Current Date : {currDate}</p>
+//   <p>Current Time : {currTime}</p>
+// </>,
+// document.getElementById("root")
+
+// );
+
+
+//Practice Problem 3 -> Creating a simple greeting website using Reactjs
+
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting = '';
+const cssStyle = {};
+
+if(curDate >=0 && curDate <12){
+  greeting = 'Good Morning!';
+  cssStyle.color = 'green';
+}else if(curDate >=12 && curDate <16){
+  greeting = 'Good Afternoon!';
+  cssStyle.color = 'brown';
+}else if(curDate >=16 && curDate <19){
+  greeting = 'Good Evening!';
+  cssStyle.color = 'orange';
+}else{
+  greeting = 'Good Night!';
+  cssStyle.color = 'skyblue';
+}
+
+ReactDOM.render(
+<>
+  <div>
+  <h1>Hello User, <span style={cssStyle}>{greeting}</span></h1>
+  </div>
+</>,
+document.getElementById("root")
+);
--- textutils/src/index.js
+++ textutils/src/index.js
@@ -1,7 +1,104 @@
-var React = require("react");
-// import React from "react";
-var ReactDOM = require("react-dom");
-// import React from 'react-dom';
+// var React = require("react");
+import React from "react";
+// var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+//import "./index.css"; //Connecting External CSS to Reactjs file.
 
-ReactDOM.render(<h1>Hello World...</h1>,
-    document.getElementById("root"));    //This render element takes three parts 'what to show', 'where to show', 'callback function'
+//In react v16 or above it is possible for render() to return an array of elements
+// So instead of <div> tag we can also use [], 
+
+
+//Topics learnt
+//1. Rendering multiple elements inside ReactDOM.render() 
+//2. React Fragment
+
+//Fragments are needed when we want to render a group of elements without a parent element or component. To use it, we either use the Fragment component or <></> for short, to just render the h1 and h2 elements without the div around them.
+
+//3. JavaScript Expressions in JSX  -> JavaScript - HTML - JavaScript{}
+//4. ES6 Template literals in JSX 
+//5. Attributes in JSX
+//6. External CSS Styling & Importing CSS files in Reactjs And also 
+//class vs className -> In React there is a reserved keyword named 'class' so instead we should use 'className'
+//7. Internal CSS & Inline CSS styling in Reactjs 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ReactDOM.render( //Rendering mltiple elements inside ReactDOM.render() -> Example using <,> tag below
+/* <div>
+<h1>Hello World... This is JSX</h1>
+<p> Lorem ipsum dolar sit </p>
+<h2> Rendering multiple elements inside ReactDOM.render() </h2>
+</div>, */
+/* <React.Fragment><h1>Hello World... This is JSX</h1> */
+/* <p> Lorem ipsum dolar sit </p> */
+/* <h2> React Fragment </h2> */
+// </React.Fragment>,   
+// document.getElementById("root"));    //This render element takes three parts 'what to show', 'where to show', 'callback function'
+
+
+// ReactDOM.render(React.createElement("h1", null, "All about JSX"),
+// document.getElementById("root"));
+
+
+const flName = "Geetha Atkuri";
+const age = 23;
+let country = 'India';
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const links = "https://picsum.photos/";
+const heading = {
+    color: "#fa9191", 
+    textTransform: "capitalize", 
+    textAlign: "center",
+    fontWeight: "bold",
+    textShadow: "0px 2px 4px #ffe9c5",
+    margin: "60px 0px",
+
+};
+
+ReactDOM.render(
+<>
+<h1>JSX in React</h1>
+<p>My name is {`${flName}`} and my age is {`${age}`}</p>
+<p>The result of the expression is {2+5}</p>
+<p>Generating random number of {Math.random()}</p>
+</>,
+document.getElementById("root")
+);
+//Inside the pair of curly braces we cannot use if,if-else,.... statements 
+
+ReactDOM.render(
+<>
+    {/* Inline styling by using style attribute inside element <h1 className="heading" style={{ color: "green", textTransform: "capitalize", textAlign: "center"}}>I am from {country}</h1> */}
+
+    {/* or the other way is creating object for an element and adding that object inside {} like below */}
+    <h1 className="heading" style={heading}>I am from {country}</h1>
+    <div className="img_div" style={{display: "flex", justifyContent: "center"}}>
+    <img src= {img1} alt="Random Images" style={{width: "300px", height: "350px"}} />
+    <img src= {img2} alt="Random Images" style={{width: "300px", height: "350px"}}/>
+    <a href= {links} target="_blank">
+    <img src= {img3} alt="Random Images" style={{width: "300px", height: "350px"}}/>
+    </a>
+    </div>
+</>, 
+document.getElementById("root")
+);
+
+
+
+//Basic JS
+// var h1 = document.createElement('h1');
+// h1.innerHTML = 'Hi, Helloworld'; 
+// document.getElementById("root").appendChild(h1);
